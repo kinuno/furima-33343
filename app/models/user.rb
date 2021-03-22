50ef265399e3625381d75af3d01725f029c8c8ca
kinuno <kinu.hirata@gmail.com>
2021-03-22 20:04:25 +0900

    ユーザー新規登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message:"Full-width characters" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message:"Full-width characters" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message:"Full-width katakana characters" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message:"Full-width katakana characters" }
+  validates :birthday, presence: true
+  validates :email, uniqueness: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message:"Include both letters and numbers" }
+
+
 end
