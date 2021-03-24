7ac4e466fb3146cb703ae547d18f97b375f5c3b1
kinuno <kinu.hirata@gmail.com>
2021-03-24 21:14:12 +0900

    バリデーションの一括り、正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
-  validates :birthday, presence: true
-  validates :email, uniqueness: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: 'Full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: 'Full-width characters' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :birthday
+  end
+
 end
