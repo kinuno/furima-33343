ab4ca879e2315ee7710a98b4463c3a179c91ca81
kinuno <kinu.hirata@gmail.com>
2021-03-28 21:58:27 +0900

    価格のバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,7 @@ class Item < ApplicationRecord
   belongs_to :category, :condition, :fee, :prefecture, :delivery_days 
   
   validates :name, :explanation, :price, :image, presence: true
-  validates :category_id, :condition_id, :fee_id, :prefecture_id, :delivery_days_id, numericality: {other_than: 1}
+  validates :category_id, :condition_id, :fee_id, :prefecture_id, :delivery_days_id, numericality: {other_than: 1 , message: 'Select'}
+  validates :price, numericality: {only_integer: true, message: 'Half-width number' }
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range'}
 end
