250fe9d4a678af857335c50fe1334e2bdf066c31
kinuno <kinu.hirata@gmail.com>
2021-04-26 20:14:42 +0900

    修正：move_to_indexとset_item

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
-  before_action :move_to_index, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @order_delivery = OrderDelivery.new
@@ -35,12 +35,11 @@ class OrdersController < ApplicationController
     )
   end
 
-  def move_to_index
-    @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
-  end
-
   def set_item
     @item = Item.find(params[:item_id])
+  end
+  
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
