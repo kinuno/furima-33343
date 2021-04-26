08894e0f440454c97a64704ffdd1ba2f784d767e
kinuno <kinu.hirata@gmail.com>
2021-04-26 23:22:23 +0900

    修正　emailのバリデーション削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :email, uniqueness: true
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: 'Full-width characters' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: 'Full-width characters' }
